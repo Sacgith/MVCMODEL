495b4d6d7ff70b2ef66f6f2cf06f12ae148a1844
Sac <algorithms2019@gmail.com>
2021-02-15 13:11:26 +0530

    Add a controller and link with router for home

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ const express=require('express');
 
 const router=express.Router();
 
+
+//controller
+const  homeController=require("../controllers/home_controller");
+
 console.log("Router Loaded");
+
+//controller
+router.get('/', homeController.home);
 
 
 module.exports=router;
