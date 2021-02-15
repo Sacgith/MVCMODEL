76ae66b726549b2a56debe5077c68bad7b45a386
Sac <algorithms2019@gmail.com>
2021-02-15 15:32:17 +0530

    added home.ejs to veiws and updated to render it from homecontroller.js

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,18 @@
-module.exports.home=function(req, res)
-{
-     return res.end('<h1>Express is up for Codial</h1>');
-}
+
+
+
+//this is sending info directly to the browser
+
+// module.exports.home=function(req, res)
+// {
+//      return res.end('<h1>Express is up for Codial</h1>');
+// }
 
 //module.exports.actionName=function(req, res){}
+
+module.exports.home=function(req, res)
+{
+    return res.render('home', {
+        title:"Home"
+    });
+}
