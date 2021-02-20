89fb87924423c799a724abe3d1f451db3ccb66a8
Sac <algorithms2019@gmail.com>
2021-02-20 15:25:18 +0530

    added manual authentication(sign in )

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,11 @@ router.get('/profile', usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
-
+//sign up
 router.post('/create',usersController.create);
+//sign in
+router.post('/create-session', usersController.createSession );
+
 
 
 
